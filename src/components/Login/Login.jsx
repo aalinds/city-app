c0223df66b4c8c5d9e2d23bcb9c6a6a36138c99f
Aalind Sharma <aalinds.sharma@gmail.com>
2021-11-10 17:03:24 +0530

    login component converted to functional

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Login.module.css';
 
-class Login extends Component {
-	render() {
-		return (
-			<div className={`d-flex ${styles.parent}`}>
-				<form className={`${styles['form-signin']}`}>
+const Login = () => {
+	return (
+		<div className={`d-flex ${styles.parent}`}>
+			<form className={`${styles['form-signin']}`}>
+				<Link to='/'>
 					<img
 						className='mb-5'
 						src='https://web.mydukaan.io/images/logo.svg'
@@ -14,28 +14,28 @@ class Login extends Component {
 						width='150'
 						height='44'
 					/>
-					<h1 className='h3 mb-5 font-weight-normal'>Log in</h1>
-					<label htmlFor='mobile' className='sr-only'>
-						Mobile number
-					</label>
-					<input
-						type='text'
-						id='mobile'
-						className={`form-control mb-4`}
-						placeholder='Enter mobile number'
-						required=''
-						autofocus=''
-					/>
-					<button
-						className={`btn btn-primary btn-lg ${styles['login-btn']}`}
-						type='submit'
-					>
-						Log in
-					</button>
-				</form>
-			</div>
-		);
-	}
-}
+				</Link>
+				<h1 className='h3 mb-5 font-weight-normal'>Log in</h1>
+				<label htmlFor='mobile' className='sr-only'>
+					Mobile number
+				</label>
+				<input
+					type='text'
+					id='mobile'
+					className={`form-control mb-4`}
+					placeholder='Enter mobile number'
+					required=''
+					autofocus=''
+				/>
+				<button
+					className={`btn btn-primary btn-lg ${styles['login-btn']}`}
+					type='submit'
+				>
+					Log in
+				</button>
+			</form>
+		</div>
+	);
+};
 
 export default Login;
